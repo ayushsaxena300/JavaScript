7b91cb7144dfa79bf6d6402d389e825052343a43
cipher <ayushsaxena300@gmail.com>
2024-06-29 00:00:13 +0530

    commit 1.4

---
+++ js-hindi-youtube/01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+// let score = 33            // "33" => 33
+// let score = "33abc"       // "33abc" => NaN(Not a Number but type is number) 
+// let score = null
+// let score = true          // true => 1; false => 0
+// let score = undefined
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// let isLoggedIn = 1        // 1 => true; 0 => false
+// let isLoggedIn = ""       // "" => false
+let isLoggedIn = "hitesh"    // "hitesh" => true
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
